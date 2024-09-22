c152dcbcea301a92e3924e191a4da98041114866
GaspardPierre <pierredillard@gmail.com>
2024-09-22 12:33:59 +0200

    update TagCloud style

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth/next"
 import { authOptions } from "../auth/[...nextauth]/authOptions"
 import { NextResponse } from "next/server"
 
+export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
   try {
     const session = await getServerSession(authOptions)
--- components/TagCloud.tsx
+++ components/TagCloud.tsx
@@ -1,17 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import {
-  BookOpen, // société
-  FlaskConical, // science
-  Vote, // politique
-  Heart, // spiritualité
-  Sparkles, // divers
-  Zap, // insolite
-  Cpu, // technologie
-  Coffee, // vie quotidienne
-  Leaf, // environnement
-  PenTool, // poésie
-} from "lucide-react";
 
 import { Tag } from "@/app/types";
 
@@ -19,65 +7,33 @@ interface TagCloudProps {
   tags: Tag[];
 }
 
-const iconMap = {
-  société: BookOpen,
-  science: FlaskConical,
-  politique: Vote,
-  spiritualité: Heart,
-  divers: Sparkles,
-  insolite: Zap,
-  technologie: Cpu,
-  "vie quotidienne": Coffee,
-  environnement: Leaf,
-  poésie: PenTool,
-};
-
-const colorMap = {
-  société: 'bg-sky-900 text-white',        // Deep sky blue for a mature feel
-  science: 'bg-indigo-800 text-white',     // Dark indigo for a serious, modern tone
-  politique: 'bg-red-700 text-white',      // Darker red for a strong, yet professional look
-  spiritualité: 'bg-purple-800 text-white', // Dark purple for a calming, balanced presence
-  divers: 'bg-emerald-800 text-white',     // Deep emerald green for a natural and refined vibe
-  insolite: 'bg-yellow-700 text-white',    // Muted golden yellow to signify importance without being too bright
-  technologie: 'bg-gray-700 text-white',   // Dark gray for a sleek, tech-focused aesthetic
-  "vie quotidienne": 'bg-pink-700 text-white', // Deep pink for a modern twist with elegance
-  environnement: 'bg-teal-800 text-white', // Dark teal to symbolize nature in a sophisticated way
-  poésie: 'bg-cyan-800 text-white'         // Deep cyan for a creative yet understated flair
-}
 
 
 
 export default function TagCloud({ tags }: TagCloudProps) {
-  const tagsWithCount = tags.map((tag) => ({
-    ...tag,
-    count: tag.count ?? tag.posts.length + tag.videos.length,
-  }));
+  const maxCount = Math.max(...tags.map(tag => tag.count || 0));
 
-  const maxCount = Math.max(...tagsWithCount.map((tag) => tag.count));
-  console.log("TagCloud is rendering");
+  const getTagSize = (count: number) => {
+    const minSize = 0.3;
+    const maxSize = 1;
+    return minSize + (count / maxCount) * (maxSize - minSize);
+  };
 
   return (
     <div className="rounded-lg p-4 sticky top-16">
-      <h2 className="text-xl font-semibold mb-4">Explorer les sujets...</h2>
+      <h2 className="text-2xl font-semibold mb-4 text-text">Explorer les sujets...</h2>
       <div className="flex flex-wrap gap-2">
-        {tags.map((tag) => {
-          const TagIcon =
-            iconMap[tag.name.toLowerCase() as keyof typeof iconMap] || Sparkles;
-          const colorClass =
-            colorMap[tag.name.toLowerCase() as keyof typeof colorMap] ||
-            "bg-gray-100 text-gray-800";
-
-          return (
-            <Link
-              key={tag.id}
-              href={`/blog/tag/${tag.slug}`}
-              className={`flex items-center px-3 py-1 rounded-sm text-sm hover:opacity-80 transition-opacity ${colorClass}`}
-            >
-              <TagIcon size={16} className="mr-1" />
-              {tag.name.toUpperCase()} ({tag.count})
-            </Link>
-          );
-        })}
+        {tags.map((tag) => (
+          <Link
+            key={tag.id}
+            href={`/blog/tag/${tag.slug}`}
+            className={`px-2 py-0 rounded-md bg-gradient-to-r from-primary-400 to-secondary-600 text-white hover:text-cyan-50 hover:to-secondary-700 transition-colors`}
+            style={{ fontSize: `${getTagSize(tag.count || 0)}rem` }}
+          >
+            <span className="font-semibold">{tag.name.toUpperCase()}</span>
+            <span className="ml-1 text-xs opacity-75">({tag.count || 0})</span>
+          </Link>
+        ))}
       </div>
     </div>
   );
